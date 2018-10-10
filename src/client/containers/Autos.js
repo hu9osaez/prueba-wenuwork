64ab092be056997583586fecd274da18a088f413
Hugo Sáez <hu9o.saez@gmail.com>
2018-10-09 23:45:15 -0300

    Base for autos and usuarios routes

--- src/client/containers/Autos.js
+++ src/client/containers/Autos.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Subtitle, Table } from 'bloomer';
 
-const Autos = () => (
-  <div>Autos</div>
-);
+export default class Autos extends React.Component {
+  state = { autos: null };
 
-export default Autos;
+  componentDidMount() {
+    fetch('/api/autos')
+      .then(res => res.json())
+      .then(autos => this.setState({ autos }));
+  }
+
+  render() {
+    const { autos } = this.state;
+    return (
+      <div>
+        <Subtitle isSize={3}>Autos</Subtitle>
+        <Table isBordered isStriped isNarrow className="is-fullwidth">
+          <thead>
+            <tr>
+              <th>Marca</th>
+              <th>Modelo</th>
+              <th>Año</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
+}
--- src/client/containers/Usuarios.js
+++ src/client/containers/Usuarios.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Subtitle, Table } from 'bloomer';
 
-const Usuarios = () => (
-  <div>Usuarios</div>
-);
+export default class Usuarios extends React.Component {
+  state = { usuarios: null };
 
-export default Usuarios;
+  componentDidMount() {
+    fetch('/api/usuarios')
+      .then(res => res.json())
+      .then(usuarios => this.setState({ usuarios }));
+  }
+
+  render() {
+    const { usuarios } = this.state;
+    return (
+      <div>
+        <Subtitle isSize={3}>Usuarios</Subtitle>
+        <Table isBordered isStriped isNarrow className="is-fullwidth">
+          <thead>
+          <tr>
+            <th>RUT</th>
+            <th>Nombre</th>
+            <th>Auto</th>
+            <th />
+          </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
+}
