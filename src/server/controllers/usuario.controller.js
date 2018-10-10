ca4f55e7ff02acb2a7223e8b75fe6b29bcf281cb
Hugo Sáez <hu9o.saez@gmail.com>
2018-10-09 22:54:10 -0300

    CRUD for users

--- src/server/controllers/usuario.controller.js
+++ src/server/controllers/usuario.controller.js
@@ -1,21 +1,52 @@
 const Usuario = require('../models/usuario.model');
 
 exports.index = (req, res) => {
-
+  Usuario.find({}).populate('auto').exec((err, usuarios) => {
+    res.json(usuarios);
+  });
 };
 
 exports.show = (req, res) => {
+  Usuario.findById(req.params.id).populate('auto').exec((err, usuario) => {
+    if (err) return res.sendStatus(400);
+    if (!usuario) return res.sendStatus(404);
 
+    return res.json(usuario);
+  });
 };
 
 exports.store = (req, res) => {
+  const usuario = new Usuario({
+    rut: req.body.rut,
+    nombre: req.body.nombre,
+    auto: req.body.auto,
+  });
 
+  usuario.save((err) => {
+    if (err) return res.sendStatus(400);
+
+    return res.json({
+      message: 'Usuario creado correctamente.'
+    });
+  });
 };
 
 exports.update = (req, res) => {
+  Usuario.findByIdAndUpdate(req.params.id, { $set: req.body }, (err) => {
+    if (err) return res.sendStatus(400);
 
+    return res.json({
+      message: 'Datos del usuario actualizados correctamente.'
+    });
+  });
 };
 
 exports.destroy = (req, res) => {
+  Usuario.findByIdAndRemove(req.params.id, (err) => {
+    if (err) return res.sendStatus(400);
 
+    return res.json({
+      message: 'Datos del usuario eliminados correctamente.'
+    });
+  });
 };
--- src/server/routes/usuarios.js
+++ src/server/routes/usuarios.js
@@ -7,7 +7,7 @@ const controller = require('../controllers/usuario.controller');
 router.get('/', controller.index);
 router.post('/', controller.store);
 router.get('/:id', controller.show);
-router.put('/:id/update', controller.update);
-router.delete('/:id/delete', controller.destroy);
+router.put('/:id', controller.update);
+router.delete('/:id', controller.destroy);
 
 module.exports = router;
