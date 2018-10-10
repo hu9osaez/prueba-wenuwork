91f7e4da284ff2ae0595dbd4aecca9b3b698788b
Hugo Sáez <hu9o.saez@gmail.com>
2018-10-09 23:06:30 -0300

    Added bulma to stylize the web

--- src/client/App.js
+++ src/client/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import './app.css';
-import ReactImage from './react.png';
+import './app.scss';
+import { Container, Box  } from 'bloomer';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <img src={ReactImage} alt="react" />
+        <Container>
+          <Box>Hello Word</Box>
+        </Container>
       </div>
     );
   }
